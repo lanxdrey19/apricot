5c2f6852f50a7fcbde195a5464743ca343560005
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-26 21:25:53 +1300

    :art: implement dao components based on folder structure

---
+++ daos/drop_dao.js
@@ -0,0 +1,22 @@
+const Template = require("../entities/Template");
+
+const getTemplates = async function () {
+  try {
+    const templates = await Template.find();
+    let finalList = [];
+    while (finalList.length < 3) {
+      const template = templates[Math.floor(Math.random() * templates.length)];
+
+      if (!finalList.includes(template)) {
+        finalList.push(template);
+      }
+    }
+    return finalList;
+  } catch (err) {
+    throw new Error("server error occurred");
+  }
+};
+
+module.exports = {
+  getTemplates,
+};
